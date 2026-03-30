17d15c86555845833e21a4c99feb4e6b0ec4018f
Gerardo Puga <glpuga@gmail.com>
2026-03-30 16:58:24 -0300

    Add iRobot's ros2-performance
    Signed-off-by: Gerardo Puga <glpuga@gmail.com>

--- docker/files/patches_ext/ros2-performance.sh
+++ docker/files/patches_ext/ros2-performance.sh
@@ -4,4 +4,5 @@ script_location=$(dirname "$(readlink -f "$0")")
 script_name=$(basename "$0")
 script_name_minus_extension="${script_name%.*}"
 
-touch COLCON_IGNORE
+# see https://github.com/irobot-ros/ros2-performance/pull/143#pullrequestreview-3290054275
+git apply ${script_location}/${script_name_minus_extension}.patch
