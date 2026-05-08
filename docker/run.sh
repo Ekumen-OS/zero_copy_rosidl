dac6ead70d25ad68a24d74df089650773fcef381
Francisco Rossi <frrossi@fi.uba.ar>
2026-05-08 20:04:35 -0300

    Rework docker container for faster builds

--- docker/run.sh
+++ docker/run.sh
@@ -24,7 +24,7 @@ cd $(dirname "$(readlink -f "$0")")
 function print_help {
     echo "Usage: $(basename $0) [-b|--build] [-s|--service SERVICE_NAME] [-h|--help] [-- COMMAND [ARGS...]]"
     echo "  --build                      Build the image before starting the container."
-    echo "  --service                    Set the service name (default: zero_rosidl_devel)."
+    echo "  --service                    Set the service name (default: unpatched_workspace). Options: unpatched_workspace, patched_workspace"
     echo "  --help                       Display this help message."
     echo "  --                           Everything after this is used as the command to execute in the container."
 }
@@ -44,7 +44,7 @@ if [[ $RET_CODE -ne 0 ]]; then
 fi
 
 BUILD=false
-SERVICE_NAME="zero_rosidl_devel"
+SERVICE_NAME="unpatched_workspace"
 
 eval set -- "$VALID_ARGS"
 while [[ "$1" != "" ]]; do
@@ -75,14 +75,6 @@ done
 
 # Capture any remaining arguments after -- as the command to execute
 COMMAND_ARGS=("$@")
-
-# if the ROS_DOMAIN_ID variable is not set, abort
-if [[ -z "${ROS_DOMAIN_ID}" ]]; then
-  echo
-  echo "The ROS_DOMAIN_ID environment variable must be set."
-  echo
-  exit 1
-fi
 
 CONTAINER_NAME="${USER}_${SERVICE_NAME}"
 
